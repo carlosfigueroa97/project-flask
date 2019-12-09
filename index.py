5c0834b61dee21013fef344be8088687a05d9247
Carlos Figueroa <cafb97@gmail.com>
2019-12-09 13:57:11 -0600

    Show image.

--- index.py
+++ index.py
@@ -6,6 +6,7 @@ from methods import *
 file2 = ""
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = './pdfs/'
+app.config['IMG_FOLDER'] = './img/'
 
 @app.route('/')
 def index():
@@ -18,7 +19,7 @@ def upload_file():
         if(request.method == "POST"):
             file = request.files['fileUpload']
             filename = secure_filename(file.filename)
-            file2 = filename    
+            file2 = filename
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
             response = read_csv(filename)
             df = response.head()
@@ -33,8 +34,9 @@ def meessage():
         columnX = request.form['columnX']
         option = request.form['option']
         nameGraph = request.form['nameGraph']
-        graph_dataframe(file2, columnY, columnX, option, nameGraph)
-        return render_template("message.html")
+        plt = graph_dataframe(file2, columnY, columnX, option, nameGraph, app)
+        image = app.config['IMG_FOLDER'] + nameGraph + ".png"
+        return render_template("message.html", filename = plt.show(), image = image)
     except SystemError as err:
         print(err)
 
@@ -43,5 +45,5 @@ def page_not_found(error):
     return "<h1>Página no encontrada</h1>"
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
     
--- methods.py
+++ methods.py
@@ -1,11 +1,12 @@
 import pandas as pd
+import os
 import matplotlib.pyplot as plt
 
 def read_csv(file):
     df = pd.read_csv('./pdfs/' + file)
     return df
 
-def graph_dataframe(route, columnY, columnX, option, nameGraph):
+def graph_dataframe(route, columnY, columnX, option, nameGraph, app):
     try:
         df = read_csv(route)
         if(option == 'barra'):
@@ -14,7 +15,8 @@ def graph_dataframe(route, columnY, columnX, option, nameGraph):
             plt.pie(df[columnY], labels=df[columnX])
         else:
             plt.plot(df[columnY], 'b--')
-        plt.savefig("{}.png".format(nameGraph))
+        plt.savefig(os.path.join(app.config['IMG_FOLDER']) + "{}.png".format(nameGraph))
+        return plt
     except SystemError as err:
         print(err)
 
